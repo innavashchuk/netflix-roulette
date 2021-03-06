8d8a3229bc1efe0984bcdb46f89fff2588cb93c7
Inna_Vashchuk <vashchuk.inna@gmail.com>
2021-03-06 20:56:33 +0200

    add usage of PropTypes

--- src/containers/movie-card/Movie-Card.tsx
+++ src/containers/movie-card/Movie-Card.tsx
@@ -70,3 +70,21 @@ export default class MovieCard extends React.Component<MovieCardProps, MovieCard
     )
   }
 }
+
+// @ts-ignore
+MovieCard.propTypes = {
+  movie: PropTypes.exact({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    tagline: PropTypes.string,
+    vote_average: PropTypes.number,
+    vote_count: PropTypes.number,
+    release_date: PropTypes.string,
+    poster_path: PropTypes.string,
+    overview: PropTypes.string,
+    budget: PropTypes.number,
+    revenue: PropTypes.number,
+    genres: PropTypes.arrayOf(PropTypes.string),
+    runtime: PropTypes.number
+  })
+}
